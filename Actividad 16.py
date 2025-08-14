df3a6be34daa224e0a42f1091ea5e40453042b86
Josehdz23 <jose.jahr.2016@gmail.com>
2025-08-13 19:58:52 -0600

    Más avances, no muy significativos

--- Actividad 16.py
+++ Actividad 16.py
@@ -1,12 +1,15 @@
 class Libros:
-    def __init__(self, titulo, autor, año, codigo):
+    def __init__(self, titulo, autor, año, prestado):
         self.titulo = titulo
         self.autor = autor
         self.año = año
-        self.codigo = codigo
+        self.prestado = prestado
 
     def __str__(self):
-        print(f"Codigo: {self.codigo} Autor: {self.autor} Año: {self.año} Titulo: {self.titulo}")
+        if self.prestado:
+            print(f"Autor: {self.autor} Año: {self.año} Titulo: {self.titulo} Prestado: Si")
+        else:
+            print(f"Autor: {self.autor} Año: {self.año} Titulo: {self.titulo} Prestado: No")
 
 class Usuarios:
     def __init__(self, nombre, carnet, carrera):
@@ -25,9 +28,36 @@ class ManipularLibros:
         try:
             while True:
                 try:
-                    pass
+                    codigo = int(input("\nCodigo del libro: "))
+                    if codigo in self.libros:
+                        print("El codigo ya está en uso!!")
+                    else:
+                        break
                 except Exception as e:
                     print(f"Error: {e}")
+            while True:
+                autor = input("Nombre autor: ")
+                if autor or autor.isspace():
+                    break
+                else:
+                    print("Nombre de autor inválido: ")
+            while True:
+                titulo = input("Título: ")
+                if titulo or titulo.isspace():
+                    break
+                else:
+                    print("El título del libro no es válido: ")
+            while True:
+                try:
+                    año = int(input("Ingrese el año de publilcación del libro: "))
+                    if año > 0:
+                        break
+                    else:
+                        print("EL año del libro no es válido: ")
+                except Exception as e:
+                    print(f"Error: {e}")
+            prestado = False
+            self.libros[codigo] = Libros(titulo, autor, año, prestado)
         except Exception as e:
             print(f"Error: {e}")
 
